609051775cb359a23cc501f3035474855c46edef
Josh Benner <josh@bennerweb.com>
2013-01-26 16:01:36 -0500

    Broken launching... not sure what's going on.

--- mudsling.py
+++ mudsling.py
@@ -1,13 +1,34 @@
 """
-This is our Twistd entry point. You could also use the normal twistd script
-and point it towards mudsling/server.py.
+Main MUDSling entry point.
+
+Typically, this script spawns two processes: server and proxy.
+
+However, if the --debug command line option is specified, this script will
+directly execute the server and override the use of the SimpleTelnetServer.
 """
+#from sys import argv
+from multiprocessing import Process
 
-from sys import argv
-from twisted.scripts.twistd import run
+#from twisted.scripts.twistd import run
 
-argv[1:] = [
-    '-y', 'mudsling/server.py'
-]
 
-run()
+def run_server():
+    #argv[1:] = [
+    #    '-y', 'mudsling/server.py'
+    #]
+
+    #run()
+    print 'server!'
+
+
+def run_proxy():
+    print "I am a PROXY!"
+
+if __name__ == '__main__':
+    print 'server process'
+    server = Process(target=run_server)
+    #proxy = Process(target=run_proxy)
+    print 'run server'
+    server.start()
+    #proxy.start()
+    server.join()
