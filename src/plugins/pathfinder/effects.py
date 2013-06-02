a392baa1c8205b65795dbc0c392e31140e601f55
Josh Benner <josh@bennerweb.com>
2013-06-02 10:23:56 -0400

    Implemented effect parser.

--- src/plugins/pathfinder/effects.py
+++ src/plugins/pathfinder/effects.py
@@ -11,24 +11,81 @@ Effect syntax: <roll expr> [{bonus|penalty}] to <stat> [<expiration>]
     * for <expr> {round|turn|second|minute|hour|day}[s]
     * until <event>
 
+BNF:
+  rollexpr ::= <diceroll grammar>
+    nature ::= <alphanums> (<printables> | " ")*
+      type :: = "bonus" | "penalty"
+      stat ::= <printables>+
+  timeunit ::= ("round" | "turn" | "second" | "minute" | "hour" | "day") ["s"]
+  duration ::= "for" rollexpr timeunit
+     event ::= <printables>+
+     until ::= "until" event
+expiration ::= duration | until
+    effect ::= rollexpr [nature] [type] "to" stat [expiration]
+
 Examples:
 * +2 to STR for 2 turns
-* +1 bonus to Attack for 1 day
+* +1d4 + 1 enhancement bonus to Attack for 1 day
 * +1d4 Damage
 """
-import re
-
 from mudsling.storage import Persistent
 
 import dice
 
-effect_re = re.compile("^(.+?)(?: +bonus| +penalty)? +to +(.+?)"
-                       "(?: +for +(.+) +(?:rounds?|turns?))?$", re.I)
+
+def _grammar():
+    from pyparsing import Optional, CaselessKeyword, Suppress, StringEnd
+    from pyparsing import SkipTo, WordStart, oneOf
+
+    CK = CaselessKeyword
+
+    to = CK("to").suppress()
+    type = (CK("bonus") | CK("penalty")).setResultsName("type")
+    nature = WordStart() + SkipTo(type | to).setResultsName("nature")
+    effecttype = Optional(nature, default=None) + Optional(type, default=None)
+    timeunits = oneOf("round rounds turn turns second seconds minute minutes "
+                      "hour hours day days", caseless=True)
+    interval = dice.grammar + timeunits
+    duration = (CK("for").suppress() + interval).setResultsName("duration")
+    until = Suppress(CK("until")) + SkipTo(StringEnd()).setResultsName("until")
+    expire = duration | until
+    stat = WordStart() + SkipTo(expire | StringEnd()).setResultsName("stat")
+    val = dice.grammar.setResultsName("effect_val")
+    effect = val + effecttype + to + stat + Optional(expire, default=None)
+
+    return effect
+
+grammar = _grammar()
 
 
 class Effect(Persistent):
-    def __init__(self, effectstr):
-        rollstr, stat, rounds = effect_re.match(effectstr).groups()
-        self.roll = dice.Roll(rollstr)
-        self.stat = stat
-        self.duration = rounds
+    """
+    Represents an effect that can be provided by equipment, conditions, class
+    features, etc (just about anything).
+
+    This class is a *stored* effect, as opposed to an effect that is currently
+    applied to an object/character.
+    """
+    roll = None
+    nature = None
+    type = None
+    stat = ''
+    expire_event = None
+    duration_roll = None
+    duration_unit = None
+
+    def __init__(self, effect, parser=None):
+        parser = parser or grammar
+        parsed = parser.parseString(effect, True)
+        self.roll = dice.Roll(parsed['effect_val'])
+        if parsed['nature'] is not None:
+            self.nature = parsed['nature'].strip()
+        if parsed['type'] is not None:
+            self.type = parsed['type'].strip()
+        self.stat = parsed['stat'].strip()
+        if 'until' in parsed:
+            self.expire_event = parsed['until']
+        if 'duration' in parsed:
+            val, unit = parsed['duration']
+            self.duration_roll = dice.Roll(val)
+            self.duration_unit = unit
