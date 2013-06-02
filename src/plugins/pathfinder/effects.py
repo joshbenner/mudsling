edac24c080c156c051ef544df8e77470cfdae2a4
Josh Benner <josh@bennerweb.com>
2013-06-01 23:25:57 -0400

    Tiny bit of docs on effects. Need to refine this concept more.

--- src/plugins/pathfinder/effects.py
+++ src/plugins/pathfinder/effects.py
@@ -2,14 +2,18 @@
 Effects are modifiers that are applied to various stats on an object. Effects
 can be provided by just about anything, and can apply to any Pathfinder object.
 
-Effect syntax: <roll expr> [{bonus|penalty}] to <stat> [for <expr> round[s]]
+Effect syntax: <roll expr> [{bonus|penalty}] to <stat> [<expiration>]
 * roll expr: The value of the effect.
 * Bonus/penalty: no impact on value of effect, just flexiblity in effect text.
 * stat: The stat of the subject impacted by the effect.
+    * <skill>|<ability> [mod[ifier]]|
+* expiration: Turns, time, or event which expires the effect.
+    * for <expr> {round|turn|second|minute|hour|day}[s]
+    * until <event>
 
 Examples:
-* +2 to STR
-* +1 bonus to Attack
+* +2 to STR for 2 turns
+* +1 bonus to Attack for 1 day
 * +1d4 Damage
 """
 import re
