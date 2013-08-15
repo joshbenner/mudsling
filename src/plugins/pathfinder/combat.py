fa9908375b460cb548f1118bce0e1e5ee83da842
Josh Benner <josh@bennerweb.com>
2013-08-15 19:23:13 -0400

    Some combat docs, added initiative calculation and sorting.

--- src/plugins/pathfinder/combat.py
+++ src/plugins/pathfinder/combat.py
@@ -1,3 +1,7 @@
+import sys
+import random
+from collections import defaultdict
+
 from mudsling.storage import Persistent
 
 from pathfinder.objects import PathfinderObject
@@ -7,17 +11,28 @@ class Battle(Persistent):
     """Tracks the rounds and turns in a battle.
 
     Turn = An individual combatant's opportunity to act during combat.
-    Round = Each combatant gets one turn in a round.
+    Round = A single set of turns, one per combatant.
+
+    :ivar combatants: The combatants participating in the battle.
+    :type combatants: list of Combatant
+
+    :ivar active_combatant_offset: The list offset of the combatant who is
+        currently taking their turn.
+    :type active_combatant_offset: int
     """
+    combatants = None
+    active_combatant_offset = None
 
     def __init__(self, combatants=()):
         """
         :type combatants: list of Combatant
         """
-        self.combatants = []
+        # Add them directly, then add them one at a time so they all receive
+        # notice of each combatant joining the fray during initialization.
+        self.combatants = list(combatants)
         for combatant in combatants:
-            self.add_combatant(combatant)
-        self.current_combatant_offset = 0
+            self.add_combatant(combatant, update_initiative=False)
+        self.update_initiative()
 
     @property
     def active(self):
@@ -30,25 +45,85 @@ class Battle(Persistent):
         willing = [c for c in capable if c.combat_willing]
         return len(willing) > 0 and len(capable) > 1
 
+    @property
+    def active_combatant(self):
+        """:rtype: Combatant"""
+        try:
+            return self.combatants[self.active_combatant_offset]
+        except IndexError:
+            return None
+
     def tell_combatants(self, *parts):
         for combatant in self.combatants:
-            pass
+            combatant.tell(*parts)
 
-    def add_combatant(self, combatant):
-        if combatant in self.combatants:
-            return
+    def add_combatant(self, combatant, update_initiative=True):
+        if combatant not in self.combatants:
+            self.combatants.append(combatant)
         combatant.battle = self
-        self.combatants.append(combatant)
+        self.tell_combatants('{c', combatant, " {yjoins the battle.")
+        if update_initiative:
+            self.update_initiative()
+
+    def update_initiative(self):
+        """Cause any new combatants to roll initiative, and sort the combatants
+        into their new combat order.
+
+        The current combatant will remain the current combatant, and new
+        combatants will be slotted into where they fit in the initiative order.
+
+        If no combatant was active, the first combatant will be activated.
+        """
+        current_combatant = self.active_combatant
+        for combatant in self.combatants:
+            if combatant.battle_initiative is None:
+                combatant.roll_initiative()
+        cmp_roll = lambda c: c.battle_initiative[0]
+        cmp_init = lambda c: c.battle_initiative[1]
+        cmp_rand = lambda c: c.battle_initiative[2]
+        combatants = sorted(self.combatants, key=cmp_roll)
+        combatants = sorted(combatants, key=cmp_init)
+        self.combatants = sorted(combatants, key=cmp_rand)
+        if current_combatant is not None:
+            for i, combatant in enumerate(self.combatants):
+                if combatant == current_combatant:
+                    self.active_combatant_offset = i
+                    break
+        else:
+            self.set_active_combatant(self.combatants[0])
+
+    def set_active_combatant(self, combatant):
+        for i in (i for i, c in enumerate(self.combatants) if c == combatant):
+            self.active_combatant_offset = i
+            return i
+        return None
 
 
 class Combatant(PathfinderObject):
     """
     Represents a battle participant.
+
+    :ivar battle: The battle the combatant is participating in.
+    :type battle: Battle or None
+
+    :ivar battle_initiative: Tuple of initiative roll, initiative bonus, and
+        a random tie-breaker value.
+    :type battle_initiative: tuple or None
+
+    :ivar combat_willing: If the combatant is willing to fight.
+    :type combat_willing: bool
+
+    :ivar combat_capable: If the combatant is capable of fighting.
+    :type combat_capable: bool
     """
     battle = None
     battle_initiative = None
     combat_willing = False
     combat_capable = True
+
+    stat_defaults = {
+        'initiative': 0,
+    }
 
     @property
     def in_combat(self):
@@ -66,3 +141,14 @@ class Combatant(PathfinderObject):
         combatants.extend(other_combatants)
         battle = Battle(combatants)
         return battle
+
+    def roll_initiative(self):
+        """Roll initiative for combat.
+
+        :return: Tuple of the rolled initiative value and two tie-breakers.
+        :rtype: tuple of int
+        """
+        self.battle_initiative = (self.roll('1d20 + initiative'),
+                                  self.get_stat('initiative'),
+                                  random.randint(0, sys.maxint))
+        return self.battle_initiative
