35a5fa43fc9593418650877a0623f069c9ba1a39
Josh Benner <josh@bennerweb.com>
2013-10-05 22:28:47 -0400

    Update some code docs from epytext to rst

--- src/mudsling/match.py
+++ src/mudsling/match.py
@@ -25,17 +25,17 @@ def match_stringlists(search, stringlists, exact=False, err=False,
 
     Strips ANSI codes and tokens from search query and all names to match.
 
-    @param search: The search string.
-    @type search: str
-    @param stringlists: Dict of lists of strings.
-    @type stringlists: dict
-    @param exact: Only look for exct matches.
-    @type exact: bool
-    @param err: If true, may raise AmbiguousMatch or FailedMatch.
-    @type err: bool
+    :param search: The search string.
+    :type search: str
+    :param stringlists: Dict of lists of strings.
+    :type stringlists: dict
+    :param exact: Only look for exct matches.
+    :type exact: bool
+    :param err: If true, may raise AmbiguousMatch or FailedMatch.
+    :type err: bool
 
-    @return: A list of 0 or more keys from stringlists.
-    @rtype: list
+    :return: A list of 0 or more keys from stringlists.
+    :rtype: list
     """
     # Lower-case search for case insensitivity.
     srch = ansi.strip_ansi(search if case_sensitive else search.lower())
@@ -73,13 +73,13 @@ def match_objlist(search, objlist, varname="names", exact=False, err=False):
     Match a search query against a list of objects using string values from the
     provided instance variable.
 
-    @param search: The search string.
-    @param objlist: An iterable containing objects to match.
-    @param varname: The instance variable on the objects to match against.
-    @param exact: Only look for exact matches.
-    @param err: If true, may raise AmbiguousMatch or FailedMatch.
+    :param search: The search string.
+    :param objlist: An iterable containing objects to match.
+    :param varname: The instance variable on the objects to match against.
+    :param exact: Only look for exact matches.
+    :param err: If true, may raise AmbiguousMatch or FailedMatch.
 
-    @rtype: list
+    :rtype: list
     """
     strings = dict(zip(objlist, map(lambda v: v() if callable(v) else v,
                                     [getattr(o, varname) for o in objlist])))
@@ -95,9 +95,9 @@ def parse_ordinal(string):
     Parses an ordinal reference at the beginning of the string, returning the
     corresponding integer and the rest of the string. Returns None on failure.
 
-    @param string: String to parse.
+    :param string: String to parse.
 
-    @rtype: tuple
+    :rtype: tuple
     """
     match = re.match(r"^(\d+)(?:st|nd|rd|th)(.*)$", string)
     if match:
@@ -116,12 +116,12 @@ def match(search, objlist, varname="names"):
     Attempt various types of matching using other functions found in match
     module.
 
-    @param search: The string to search for among the objects.
-    @param objlist: The objects to search.
-    @param varname: The name of the instance variable containing the string(s)
+    :param search: The string to search for among the objects.
+    :param objlist: The objects to search.
+    :param varname: The name of the instance variable containing the string(s)
         to search against.
 
-    @rtype: list
+    :rtype: list
     """
     matches = match_objlist(search, objlist, varname)
     if matches:
@@ -142,22 +142,22 @@ def match_failed(matches, search=None, search_for=None, show=False):
     search for a single match has failed and return False if the search
     succeeded.
 
-    @param matches: The result of the match search.
-    @type matches: list
+    :param matches: The result of the match search.
+    :type matches: list
 
-    @param search: The string used to search.
-    @type search: str
+    :param search: The string used to search.
+    :type search: str
 
-    @param search_for: A string describing what type of thing was being
+    :param search_for: A string describing what type of thing was being
         searched for. This should be the singular form of the word.
-    @type search_for: str
+    :type search_for: str
 
-    @param show: If true, will show the list of possible matches in the
+    :param show: If true, will show the list of possible matches in the
         case of an ambiguous match.
-    @type show: bool
+    :type show: bool
 
-    @return: Message if match failed, or False if match did not fail.
-    @rtype: C{str} or C{bool}
+    :return: Message if match failed, or False if match did not fail.
+    :rtype: str or bool
     """
     if len(matches) == 1:
         return False
