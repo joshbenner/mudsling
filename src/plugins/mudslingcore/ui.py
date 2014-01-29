2ebc81b055e9f2a3a3964e32dae294985f71ad15
Josh Benner <josh@bennerweb.com>
2014-01-29 17:28:44 -0500

    Add color to classic UI's h2

--- src/plugins/mudslingcore/ui.py
+++ src/plugins/mudslingcore/ui.py
@@ -234,6 +234,7 @@ class ClassicUI(BaseUI):
     width = 100
     _hr = '{c' + ('-=' * 50)  # Half width because two chars.
     h1_format = "{{y{text}"
+    h2_format = "{{y{text}"
     label_format = "{{c{text}{{y:"
     body_prefix = ' '
     body_suffix = ' '
