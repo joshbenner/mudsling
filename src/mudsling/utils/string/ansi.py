c8a62d5ccd541a08fb7ee1607976f8b0066f3a79
Josh Benner <josh@bennerweb.com>
2014-03-15 13:10:51 -0400

    Corrected and improved xterm256 support.

--- src/mudsling/utils/string/ansi.py
+++ src/mudsling/utils/string/ansi.py
@@ -178,18 +178,17 @@ class ANSIParser(object):
 
         # xterm256 {123, %c134,
         self.xterm256_map = [
-            #(r'%c([0-5]{3})', self.parse_rgb),   # %c123 - foreground colour
-            #(r'%c(b[0-5]{3})', self.parse_rgb),  # %cb123 - background colour
-            (r'{([0-5]{3})', self.parse_rgb),    # {123 - foreground colour
-            (r'{(b[0-5]{3})', self.parse_rgb)    # {b123 - background colour
+            (r'{([0-9]{3})', self.parse_xterm256),  # {123 - foreground color
+            (r'{(b[0-9]{3})', self.parse_xterm256)  # {b123 - background color
         ]
 
         # prepare regex matching
         self.ansi_sub = [(re.compile(sub[0], re.DOTALL), sub[1])
                          for sub in self.ext_ansi_map]
         self.ansi_map = self.ext_ansi_map
-        self.xterm256_sub = [(re.compile(sub[0], re.DOTALL), sub[1])
-                             for sub in self.xterm256_map]
+        self.xterm256_sub = [(re.compile(r, re.DOTALL), f)
+                             for r, f in self.xterm256_map]
+        self.xterm256_regex = re.compile(r'\{(b?\d{1,3})', re.DOTALL)
 
         # prepare matching ansi codes overall
         self.ansi_regex = re.compile("\033\[[0-9;]+m")
@@ -215,6 +214,324 @@ class ANSIParser(object):
             r'\{(?:_(?P<bg>[rgybmcwx])'
             r'|(?P<fg>[rRgGyYbBmMcCwWxX])'
             r'|(?P<special>[nui]))')
+
+        self.ansi_hex = [
+            # Primary 3-bit (8 colors).
+            ('00',  '000000'),
+            ('01',  '800000'),
+            ('02',  '008000'),
+            ('03',  '808000'),
+            ('04',  '000080'),
+            ('05',  '800080'),
+            ('06',  '008080'),
+            ('07',  'c0c0c0'),
+
+            # Equivalent "bright" versions of original 8 colors.
+            ('08',  '808080'),
+            ('09',  'ff0000'),
+            ('10',  '00ff00'),
+            ('11',  'ffff00'),
+            ('12',  '0000ff'),
+            ('13',  'ff00ff'),
+            ('14',  '00ffff'),
+            ('15',  'ffffff'),
+
+            # Strictly ascending.
+            ('16',  '000000'),
+            ('17',  '00005f'),
+            ('18',  '000087'),
+            ('19',  '0000af'),
+            ('20',  '0000d7'),
+            ('21',  '0000ff'),
+            ('22',  '005f00'),
+            ('23',  '005f5f'),
+            ('24',  '005f87'),
+            ('25',  '005faf'),
+            ('26',  '005fd7'),
+            ('27',  '005fff'),
+            ('28',  '008700'),
+            ('29',  '00875f'),
+            ('30',  '008787'),
+            ('31',  '0087af'),
+            ('32',  '0087d7'),
+            ('33',  '0087ff'),
+            ('34',  '00af00'),
+            ('35',  '00af5f'),
+            ('36',  '00af87'),
+            ('37',  '00afaf'),
+            ('38',  '00afd7'),
+            ('39',  '00afff'),
+            ('40',  '00d700'),
+            ('41',  '00d75f'),
+            ('42',  '00d787'),
+            ('43',  '00d7af'),
+            ('44',  '00d7d7'),
+            ('45',  '00d7ff'),
+            ('46',  '00ff00'),
+            ('47',  '00ff5f'),
+            ('48',  '00ff87'),
+            ('49',  '00ffaf'),
+            ('50',  '00ffd7'),
+            ('51',  '00ffff'),
+            ('52',  '5f0000'),
+            ('53',  '5f005f'),
+            ('54',  '5f0087'),
+            ('55',  '5f00af'),
+            ('56',  '5f00d7'),
+            ('57',  '5f00ff'),
+            ('58',  '5f5f00'),
+            ('59',  '5f5f5f'),
+            ('60',  '5f5f87'),
+            ('61',  '5f5faf'),
+            ('62',  '5f5fd7'),
+            ('63',  '5f5fff'),
+            ('64',  '5f8700'),
+            ('65',  '5f875f'),
+            ('66',  '5f8787'),
+            ('67',  '5f87af'),
+            ('68',  '5f87d7'),
+            ('69',  '5f87ff'),
+            ('70',  '5faf00'),
+            ('71',  '5faf5f'),
+            ('72',  '5faf87'),
+            ('73',  '5fafaf'),
+            ('74',  '5fafd7'),
+            ('75',  '5fafff'),
+            ('76',  '5fd700'),
+            ('77',  '5fd75f'),
+            ('78',  '5fd787'),
+            ('79',  '5fd7af'),
+            ('80',  '5fd7d7'),
+            ('81',  '5fd7ff'),
+            ('82',  '5fff00'),
+            ('83',  '5fff5f'),
+            ('84',  '5fff87'),
+            ('85',  '5fffaf'),
+            ('86',  '5fffd7'),
+            ('87',  '5fffff'),
+            ('88',  '870000'),
+            ('89',  '87005f'),
+            ('90',  '870087'),
+            ('91',  '8700af'),
+            ('92',  '8700d7'),
+            ('93',  '8700ff'),
+            ('94',  '875f00'),
+            ('95',  '875f5f'),
+            ('96',  '875f87'),
+            ('97',  '875faf'),
+            ('98',  '875fd7'),
+            ('99',  '875fff'),
+            ('100', '878700'),
+            ('101', '87875f'),
+            ('102', '878787'),
+            ('103', '8787af'),
+            ('104', '8787d7'),
+            ('105', '8787ff'),
+            ('106', '87af00'),
+            ('107', '87af5f'),
+            ('108', '87af87'),
+            ('109', '87afaf'),
+            ('110', '87afd7'),
+            ('111', '87afff'),
+            ('112', '87d700'),
+            ('113', '87d75f'),
+            ('114', '87d787'),
+            ('115', '87d7af'),
+            ('116', '87d7d7'),
+            ('117', '87d7ff'),
+            ('118', '87ff00'),
+            ('119', '87ff5f'),
+            ('120', '87ff87'),
+            ('121', '87ffaf'),
+            ('122', '87ffd7'),
+            ('123', '87ffff'),
+            ('124', 'af0000'),
+            ('125', 'af005f'),
+            ('126', 'af0087'),
+            ('127', 'af00af'),
+            ('128', 'af00d7'),
+            ('129', 'af00ff'),
+            ('130', 'af5f00'),
+            ('131', 'af5f5f'),
+            ('132', 'af5f87'),
+            ('133', 'af5faf'),
+            ('134', 'af5fd7'),
+            ('135', 'af5fff'),
+            ('136', 'af8700'),
+            ('137', 'af875f'),
+            ('138', 'af8787'),
+            ('139', 'af87af'),
+            ('140', 'af87d7'),
+            ('141', 'af87ff'),
+            ('142', 'afaf00'),
+            ('143', 'afaf5f'),
+            ('144', 'afaf87'),
+            ('145', 'afafaf'),
+            ('146', 'afafd7'),
+            ('147', 'afafff'),
+            ('148', 'afd700'),
+            ('149', 'afd75f'),
+            ('150', 'afd787'),
+            ('151', 'afd7af'),
+            ('152', 'afd7d7'),
+            ('153', 'afd7ff'),
+            ('154', 'afff00'),
+            ('155', 'afff5f'),
+            ('156', 'afff87'),
+            ('157', 'afffaf'),
+            ('158', 'afffd7'),
+            ('159', 'afffff'),
+            ('160', 'd70000'),
+            ('161', 'd7005f'),
+            ('162', 'd70087'),
+            ('163', 'd700af'),
+            ('164', 'd700d7'),
+            ('165', 'd700ff'),
+            ('166', 'd75f00'),
+            ('167', 'd75f5f'),
+            ('168', 'd75f87'),
+            ('169', 'd75faf'),
+            ('170', 'd75fd7'),
+            ('171', 'd75fff'),
+            ('172', 'd78700'),
+            ('173', 'd7875f'),
+            ('174', 'd78787'),
+            ('175', 'd787af'),
+            ('176', 'd787d7'),
+            ('177', 'd787ff'),
+            ('178', 'd7af00'),
+            ('179', 'd7af5f'),
+            ('180', 'd7af87'),
+            ('181', 'd7afaf'),
+            ('182', 'd7afd7'),
+            ('183', 'd7afff'),
+            ('184', 'd7d700'),
+            ('185', 'd7d75f'),
+            ('186', 'd7d787'),
+            ('187', 'd7d7af'),
+            ('188', 'd7d7d7'),
+            ('189', 'd7d7ff'),
+            ('190', 'd7ff00'),
+            ('191', 'd7ff5f'),
+            ('192', 'd7ff87'),
+            ('193', 'd7ffaf'),
+            ('194', 'd7ffd7'),
+            ('195', 'd7ffff'),
+            ('196', 'ff0000'),
+            ('197', 'ff005f'),
+            ('198', 'ff0087'),
+            ('199', 'ff00af'),
+            ('200', 'ff00d7'),
+            ('201', 'ff00ff'),
+            ('202', 'ff5f00'),
+            ('203', 'ff5f5f'),
+            ('204', 'ff5f87'),
+            ('205', 'ff5faf'),
+            ('206', 'ff5fd7'),
+            ('207', 'ff5fff'),
+            ('208', 'ff8700'),
+            ('209', 'ff875f'),
+            ('210', 'ff8787'),
+            ('211', 'ff87af'),
+            ('212', 'ff87d7'),
+            ('213', 'ff87ff'),
+            ('214', 'ffaf00'),
+            ('215', 'ffaf5f'),
+            ('216', 'ffaf87'),
+            ('217', 'ffafaf'),
+            ('218', 'ffafd7'),
+            ('219', 'ffafff'),
+            ('220', 'ffd700'),
+            ('221', 'ffd75f'),
+            ('222', 'ffd787'),
+            ('223', 'ffd7af'),
+            ('224', 'ffd7d7'),
+            ('225', 'ffd7ff'),
+            ('226', 'ffff00'),
+            ('227', 'ffff5f'),
+            ('228', 'ffff87'),
+            ('229', 'ffffaf'),
+            ('230', 'ffffd7'),
+            ('231', 'ffffff'),
+
+            # Gray-scale range.
+            ('232', '080808'),
+            ('233', '121212'),
+            ('234', '1c1c1c'),
+            ('235', '262626'),
+            ('236', '303030'),
+            ('237', '3a3a3a'),
+            ('238', '444444'),
+            ('239', '4e4e4e'),
+            ('240', '585858'),
+            ('241', '626262'),
+            ('242', '6c6c6c'),
+            ('243', '767676'),
+            ('244', '808080'),
+            ('245', '8a8a8a'),
+            ('246', '949494'),
+            ('247', '9e9e9e'),
+            ('248', 'a8a8a8'),
+            ('249', 'b2b2b2'),
+            ('250', 'bcbcbc'),
+            ('251', 'c6c6c6'),
+            ('252', 'd0d0d0'),
+            ('253', 'dadada'),
+            ('254', 'e4e4e4'),
+            ('255', 'eeeeee'),
+        ]
+
+        # Table where offset is ANSI number referring to tuple of RGB integers.
+        self.ansi_rgb = tuple(
+            (int(h, 16) & 255,
+             (int(h, 16) >> 8) & 255,
+             (int(h, 16) >> 16) & 255)
+            for i, h in self.ansi_hex
+        )
+
+        self.simplify_fg = (
+            ANSI_BLACK,
+            ANSI_RED,
+            ANSI_GREEN,
+            ANSI_YELLOW,
+            ANSI_BLUE,
+            ANSI_MAGENTA,
+            ANSI_CYAN,
+            ANSI_WHITE,
+            ANSI_HILITE + ANSI_BLACK,
+            ANSI_HILITE + ANSI_RED,
+            ANSI_HILITE + ANSI_GREEN,
+            ANSI_HILITE + ANSI_YELLOW,
+            ANSI_HILITE + ANSI_BLUE,
+            ANSI_HILITE + ANSI_MAGENTA,
+            ANSI_HILITE + ANSI_CYAN,
+            ANSI_HILITE + ANSI_WHITE
+        )
+
+        self.simplify_bg = (
+            ANSI_BACK_BLACK,
+            ANSI_BACK_RED,
+            ANSI_BACK_GREEN,
+            ANSI_BACK_YELLOW,
+            ANSI_BACK_BLUE,
+            ANSI_BACK_MAGENTA,
+            ANSI_BACK_CYAN,
+            ANSI_BACK_WHITE,
+            # Repeat, because we don't have hilite backgrounds.
+            ANSI_BACK_BLACK,
+            ANSI_BACK_RED,
+            ANSI_BACK_GREEN,
+            ANSI_BACK_YELLOW,
+            ANSI_BACK_BLUE,
+            ANSI_BACK_MAGENTA,
+            ANSI_BACK_CYAN,
+            ANSI_BACK_WHITE
+        )
+
+        # Generated lookup table to ANSI256 to ANSI16 simplification.
+        self.ansi256_to_ansi16 = tuple(
+            self._simplify_xterm256(i) for i in xrange(len(self.ansi_rgb))
+        )
 
     def token_to_state(self, token):
         """
@@ -253,6 +570,49 @@ class ANSIParser(object):
             else:
                 state = state.update(self.token_to_state(token))
         return state
+
+    def _simplify_xterm256(self, colornum):
+        """
+        Low-level function to simplify an xterm256 color integer into the
+        ANSI256 integer for the corresponding ANSI16 color (0-15).
+        """
+        tbl = self.ansi_rgb
+        red, green, blue = tbl[colornum]
+        smallest_distance = 10000000000.0
+        best_match = 0
+        for c in xrange(16):
+            cr, cg, cb = tbl[c]
+            d = pow(cr - red, 2) + pow(cg - green, 2) + pow(cb - blue, 2)
+            if d < smallest_distance:
+                smallest_distance = d
+                best_match = c
+        return best_match
+
+    def parse_xterm256(self, colornum):
+        bg = False
+        if not isinstance(colornum, int):
+            if not colornum:
+                return ''
+            if not isinstance(colornum, str):
+                colornum = colornum.groups()[0]
+            bg = colornum.startswith('b')
+            colornum = int(colornum[1 if bg else 0:])
+        return ANSI_ESCAPE + ('[%d;5;%dm' % (48 if bg else 38, colornum))
+
+    def xterm256_as_ansi16(self, match):
+        """
+        Regex replacement callback to replace an xterm256 token with its
+        best ANSI16 match.
+        """
+        if not match:
+            return ''
+        input = match.groups()[0]
+        if not input:
+            return ''
+        background = input.startswith('b')
+        xterm256num = int(input[1 if background else 0:])
+        codes = self.simplify_bg if background else self.simplify_fg
+        return codes[self.ansi256_to_ansi16[xterm256num]]
 
     def parse_rgb(self, rgbmatch):
         """
@@ -351,7 +711,6 @@ class ANSIParser(object):
         if not string:
             return ''
         self.do_xterm256 = xterm256
-        #string = utils.to_str(string)
 
         # go through all available mappings and translate them
         parts = self.ansi_escapes.split(string) + [" "]
@@ -376,7 +735,6 @@ class ANSIParser(object):
         if not string:
             return ''
         self.do_xterm256 = xterm256
-        #string = utils.to_str(string)
 
         # go through all available mappings and translate them
         parts = self.ansi_escapes.split(string) + [" "]
@@ -384,6 +742,35 @@ class ANSIParser(object):
         for part, sep in zip(parts[::2], parts[1::2]):
             for sub in self.xterm256_sub:
                 part = sub[0].sub(sub[1], part)
+            for sub in self.ansi_map:
+                part = part.replace(sub[0], sub[1])
+            string += "%s%s" % (part, sep[0].strip())
+
+        if strip_ansi:
+            # remove all ansi codes (including those manually inserted)
+            string = self.ansi_regex.sub("", string)
+
+        return string
+
+    # Efficient, handles xterm256 properly.
+    def parse_ansi3(self, string, strip_ansi=False, xterm256=False):
+        """
+        Parses a string, subbing color codes according to
+        the stored mapping.
+
+        strip_ansi flag instead removes all ansi markup.
+        """
+        if not string:
+            return ''
+        xterm256_regex = self.xterm256_regex
+        xterm256_replace = (self.parse_xterm256 if xterm256
+                            else self.xterm256_as_ansi16)
+
+        # go through all available mappings and translate them
+        parts = self.ansi_escapes.split(string) + [" "]
+        string = ""
+        for part, sep in zip(parts[::2], parts[1::2]):
+            part = xterm256_regex.sub(xterm256_replace, part)
             for sub in self.ansi_map:
                 part = part.replace(sub[0], sub[1])
             string += "%s%s" % (part, sep[0].strip())
@@ -525,7 +912,7 @@ def parse_ansi(string, strip_ansi=False, parser=ANSI_PARSER, xterm256=False):
     Parses a string, subbing color codes as needed.
     @rtype: str
     """
-    return parser.parse_ansi2(string, strip_ansi=strip_ansi, xterm256=xterm256)
+    return parser.parse_ansi3(string, strip_ansi=strip_ansi, xterm256=xterm256)
 
 
 def strip_ansi(string, parser=ANSI_PARSER):
