1753341d93db73c11de1103435eff274024b0322
Josh Benner <josh@bennerweb.com>
2014-10-15 15:16:32 -0400

    Added API key authorizations and commands to manage them.

--- plugins/mudslingcore/commands/apikeys.py
+++ plugins/mudslingcore/commands/apikeys.py
@@ -1,7 +1,7 @@
 
 from mudsling.commands import Command
 from mudsling.locks import Lock
-from mudsling.parsers import MatchObject
+from mudsling.parsers import MatchObject, StringListStaticParser
 from mudsling.objects import BasePlayer
 
 import restserver
@@ -47,17 +47,16 @@ class APIKeysCmd(Command):
                 player = actor
             keys = restserver.get_player_api_keys(player)
             title = actor.name_for(player)
+        footer = ' {ySee more info with @apikey <id>'
         actor.msg(ui.report('API Keys for %s' % title,
-                            self.key_table(actor, keys)))
+                            self.key_table(actor, keys), footer))
 
     def key_table(self, actor, keys):
-        secret = lambda k: '{r--REVOKED--' if not k.valid else k.key
         col = ui.Column
         table = ui.Table([
             col('Owner', align='l', data_key='player',
                 cell_formatter=actor.name_for),
             col('ID', align='l', data_key='id'),
-            col('Secret', align='l', cell_formatter=secret),
             col('Date Issued', align='l', data_key='date_issued',
                 cell_formatter=ui.format_timestamp,
                 formatter_args=('short',))
@@ -75,22 +74,26 @@ class APIKeyCmd(Command):
     aliases = ('@apikey', '@apikey-info', '@show-apikey')
     syntax = '<apikey>'
     arg_parsers = {'apikey': restserver.get_api_key}
-    lock = administer_api
+    lock = use_api
 
     def run(self, actor, apikey):
         """
         :type actor: mudslingcore.object.Player
         :type apikey: restserver.APIKey
         """
-        # noinspection PyTypeChecker
-        table = ui.keyval_table((
-            ('Owner', actor.name_for(apikey.player)),
-            ('ID', apikey.id),
-            ('Secret', apikey.key),
-            ('Date Issued', ui.format_timestamp(apikey.date_issued)),
-            ('Valid', '{gYes' if apikey.valid else '{rNo')
-        ))
-        actor.msg(ui.report('API Key Details for %s' % apikey.id, table))
+        if apikey.player == actor or actor.has_perm('administer api'):
+            # noinspection PyTypeChecker
+            table = ui.keyval_table((
+                ('Owner', actor.name_for(apikey.player)),
+                ('ID', apikey.id),
+                ('Secret', apikey.key),
+                ('Date Issued', ui.format_timestamp(apikey.date_issued)),
+                ('Valid', '{gYes' if apikey.valid else '{rNo'),
+                ('Authorizations', ', '.join(apikey.authorizations))
+            ))
+            actor.msg(ui.report('API Key Details for %s' % apikey.id, table))
+        else:
+            raise self._err('Permission denied.')
 
 
 class APIKeyAddCmd(Command):
@@ -115,13 +118,13 @@ class APIKeyAddCmd(Command):
                    player, '{n.')
 
 
-class APIKeyRevokeCmd(Command):
+class APIKeyInvalidateCmd(Command):
     """
-    @apikey-revoke <id>
+    @apikey-invalidate <id>
 
-    Revoke an API Key.
+    Invalidate an API Key.
     """
-    aliases = ('@apikey-revoke', '@revoke-apikey')
+    aliases = ('@apikey-invalidate', '@invalidate-apikey')
     syntax = '<apikey>'
     arg_parsers = {'apikey': restserver.get_api_key}
     lock = administer_api
@@ -137,3 +140,54 @@ class APIKeyRevokeCmd(Command):
                        ' has been revoked.')
         else:
             raise self._err('Key is already invalid.')
+
+
+class APIKeyGrantCmd(Command):
+    """
+    @apikey-grant <auth>[, <auth>, ...] to <id>
+
+    Grant authorizations (permissions) to the specified API Key.
+    """
+    aliases = ('@apikey-grant',)
+    syntax = '<authorizations> to <apikey>'
+    arg_parsers = {
+        'authorizations': StringListStaticParser,
+        'apikey': restserver.get_api_key
+    }
+    lock = administer_api
+
+    def run(self, actor, authorizations, apikey):
+        grant = []
+        for auth in authorizations:
+            if not apikey.is_authorized(auth):
+                apikey.grant_authorization(auth)
+                grant.append(auth)
+        actor.tell('Granted authorizations ({m', '{n, {m'.join(grant), '{n)',
+                   ' to {c', apikey.player, "{n's API key ({y", apikey.id,
+                   '{n).')
+
+
+class APIKeyRevokeCmd(Command):
+    """
+    @apikey-revoke <auth>[, <auth>, ...] from <id>
+
+    Revoke authorizations (permissions) from the specified API key.
+    """
+    aliases = ('@apikey-revoke',)
+    syntax = '<authorizations> from <apikey>'
+    arg_parsers = {
+        'authorizations': StringListStaticParser,
+        'apikey': restserver.get_api_key
+    }
+    lock = administer_api
+
+    def run(self, actor, authorizations, apikey):
+        revoke = []
+        for auth in authorizations:
+            if apikey.is_authorized(auth):
+                apikey.revoke_authorization(auth)
+                revoke.append(auth)
+        actor.tell('Revoked authorizations ({m', '{n, {m'.join(revoke), '{n)',
+                   ' from {c', apikey.player, "{n's API key ({y", apikey.id,
+                   '{n).')
+
--- plugins/restserver/__init__.py
+++ plugins/restserver/__init__.py
@@ -38,20 +38,23 @@ class InvalidAPIKey(FailedMatch):
     pass
 
 
-def authenticate(func):
+def authenticate(*authorizations):
     """
     Decorator to flag a method as requiring authentication.
     """
-    def func_wrapper(service, request, *a, **kw):
-        if not authenticate_request(request):
-            msg = 'Access Denied to %s %s' % (request.method, request.uri)
-            return Response(403, msg,
-                            headers={'Content-Type': MediaType.TEXT_PLAIN})
-        return func(service, request, *a, **kw)
-    return func_wrapper
+    def decorator(f):
+        def func_wrapper(service, request, *a, **kw):
+            if not authenticate_request(request,
+                                        authorizations=authorizations):
+                msg = 'Access Denied to %s %s' % (request.method, request.uri)
+                return Response(403, msg,
+                                headers={'Content-Type': MediaType.TEXT_PLAIN})
+            return f(service, request, *a, **kw)
+        return func_wrapper
+    return decorator
 
 
-def authenticate_request(request):
+def authenticate_request(request, authorizations=()):
     """
     Called for a request that requires authentication via API key signature.
 
@@ -71,27 +74,34 @@ def authenticate_request(request):
     if signature is not None:
         (key_id, nonce, signed) = signature.split(':')
         try:
+            #: :type: restserver.APIKey
             apikey = get_api_key(key_id)
         except InvalidAPIKey:
             pass  # Fall through to Access Denied
         else:
-            request.apikey = apikey
-            date_header = request.getHeader('Date')
-            if apikey.valid and date_header:
-                timestamp = stringToDatetime(date_header)
-                offset = abs(unixtime() - timestamp)
-                if offset <= 600:
-                    tosign = '\n'.join((
-                        request.method,
-                        request.uri,
-                        date_header,
-                        nonce
-                    ))
-                    hashed = hmac.new(apikey.key, tosign, sha1)
-                    verify = hashed.digest().encode('base64')
-                    if verify == signed:
-                        request.authenticated = True
-                        return True
+            authorized = True
+            for authorization in authorizations:
+                if not apikey.is_authorized(authorization):
+                    authorized = False
+                    break
+            if authorized:
+                request.apikey = apikey
+                date_header = request.getHeader('Date')
+                if apikey.valid and date_header:
+                    timestamp = stringToDatetime(date_header)
+                    offset = abs(unixtime() - timestamp)
+                    if offset <= 600:
+                        tosign = '\n'.join((
+                            request.method,
+                            request.uri,
+                            date_header,
+                            nonce
+                        ))
+                        hashed = hmac.new(apikey.key, tosign, sha1)
+                        verify = hashed.digest().encode('base64')
+                        if verify == signed:
+                            request.authenticated = True
+                            return True
     return False
 
 
@@ -122,6 +132,7 @@ class APIKey(object):
     key = None
     valid = False
     date_issued = None
+    authorizations = ()
 
     def __init__(self, player):
         self.player = player.ref()
@@ -129,3 +140,14 @@ class APIKey(object):
         self.key = random_string(32)
         self.valid = True
         self.date_issued = nowutc()
+
+    def grant_authorization(self, authorization):
+        self.authorizations += (authorization.lower(),)
+
+    def revoke_authorization(self, authorization):
+        authorization = authorization.lower()
+        self.authorizations = tuple(a for a in self.authorizations
+                                    if a != authorization)
+
+    def is_authorized(self, authorization):
+        return authorization.lower() in self.authorizations
