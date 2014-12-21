7a3f78485fc41598e3d29ef21b68101bca0d3e7d
Josh Benner <josh@bennerweb.com>
2014-12-20 22:31:31 -0500

    Modify rest server to avoid unicode when parsing JSON for now.

--- plugins/restserver/restserver_plugin.py
+++ plugins/restserver/restserver_plugin.py
@@ -1,20 +1,71 @@
+import json
+import yaml
+from xml.etree import ElementTree
+
 from twisted.application.internet import TCPServer, SSLServer
+from twisted.web.resource import Resource
 from twisted.web.server import Site
 from twisted.internet import ssl
 from OpenSSL import SSL
+
+from corepost.routing import RequestRouter
+from corepost.enums import Http, HttpHeader, MediaType
 
 import mudsling
 from mudsling.config import config
 from mudsling.extensibility import TwistedServicePlugin
 from mudsling.utils.sequence import flatten
+from mudsling.utils.serialization import json_decode_dict
 
 import restserver
 from restserver import RESTResource, RESTService, route
 
 
+class RequestRouterASCII(RequestRouter):
+    """Custom implementation to avoid JSON Unicode."""
+
+    def __init__(self, restServiceContainer, schema=None, filters=()):
+        RequestRouter.__init__(self, restServiceContainer, schema, filters)
+        self._decoder = json.JSONDecoder(object_pairs_hook=json_decode_dict)
+
+    def _RequestRouter__parseRequestData(self, request):
+        """Automatically parses JSON,XML,YAML if present"""
+        ct = HttpHeader.CONTENT_TYPE
+        if (request.method in (Http.POST, Http.PUT)
+                and ct in request.received_headers.keys()):
+            contentType = request.received_headers["content-type"]
+            if contentType == MediaType.APPLICATION_JSON:
+                try:
+                    # request.json = json.loads(request.content.read())
+                    request.json = self._decoder.decode(request.content.read())
+                except Exception as ex:
+                    raise TypeError("Unable to parse JSON body: %s" % ex)
+            elif contentType in (
+                    MediaType.APPLICATION_XML, MediaType.TEXT_XML):
+                try:
+                    request.xml = ElementTree.XML(request.content.read())
+                except Exception as ex:
+                    raise TypeError("Unable to parse XML body: %s" % ex)
+            elif contentType == MediaType.TEXT_YAML:
+                try:
+                    request.yaml = yaml.safe_load(request.content.read())
+                except Exception as ex:
+                    raise TypeError("Unable to parse YAML body: %s" % ex)
+
+
+class RESTResourceASCII(RESTResource):
+    """Custom implementation to avoid JSON Unicode."""
+
+    def __init__(self, services=(), schema=None, filters=()):
+        self.services = services
+        self._RESTResource__router = RequestRouterASCII(self, schema, filters)
+        # Skip immediate parent on purpose.
+        Resource.__init__(self)
+
+
 class RESTServer(Site):
     def __init__(self, services, *a, **kw):
-        Site.__init__(self, RESTResource(flatten(services), *a, **kw))
+        Site.__init__(self, RESTResourceASCII(flatten(services), *a, **kw))
 
     def getResourceFor(self, request):
         request.setHeader('Server', 'MUDSling/%s' % mudsling.version)
