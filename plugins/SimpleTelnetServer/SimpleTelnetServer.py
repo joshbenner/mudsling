f58a45dbe668b7892b5b3a1610e9e5cf4f702b9c
Josh Benner <josh@bennerweb.com>
2015-01-03 12:06:29 -0500

    Added MXP support to SimpleTelnetServer

--- plugins/SimpleTelnetServer/SimpleTelnetServer.py
+++ plugins/SimpleTelnetServer/SimpleTelnetServer.py
@@ -1,10 +1,12 @@
-from twisted.conch.telnet import StatefulTelnetProtocol
+from twisted.conch.telnet import Telnet, StatefulTelnetProtocol
+from twisted.protocols import basic
 from twisted.internet.protocol import ServerFactory
 from twisted.application import internet
 from twisted.internet.defer import Deferred
 
 from mudsling.extensibility import TwistedServicePlugin
 from mudsling.sessions import Session
+from mudsling.utils.string import mxp
 
 
 class SimpleTelnetServer(TwistedServicePlugin):
@@ -17,24 +19,31 @@ class SimpleTelnetServer(TwistedServicePlugin):
         return service
 
 
-class SimpleTelnetSession(StatefulTelnetProtocol, Session):
+class SimpleTelnetSession(Telnet, basic.LineReceiver, Session):
 
     #: @ivar: Will be set by factory when Session instance is spawned.
     #: @type: ServerFactory
     factory = None
+    delimiter = '\n'
 
     def connectionMade(self):
         self.game = self.factory.game
         self.open_session()
 
+    def attach_to_player(self, player):
+        super(SimpleTelnetSession, self).attach_to_player(player)
+        self.negotiate_mxp()
+
     def connectionLost(self, reason):
         self.session_closed()
 
+    def applicationDataReceived(self, bytes):
+        basic.LineReceiver.dataReceived(self, bytes)
+
     def lineReceived(self, line):
         """
-        LineReceiver (part of StatefulTelnetProtocol) calls this when a line of
-        input is received from the client. We let the Session side of things
-        handle this.
+        LineReceiver calls this when a line of input is received from the
+        client. We let the Session side of things handle this.
         """
         self.receive_input(line)
 
@@ -49,3 +58,27 @@ class SimpleTelnetSession(StatefulTelnetProtocol, Session):
     def disconnect(self, reason):
         self.send_output(reason)
         self.transport.loseConnection()
+
+    def enableRemote(self, option):
+        return False
+
+    def enableLocal(self, option):
+        if option == mxp.TELNET_OPT:
+            self.set_option('mxp', True)
+            return True
+        return False
+
+    def disableLocal(self, option):
+        if option == mxp.TELNET_OPT:
+            self.set_option('mxp', False)
+            return True
+        return False
+
+    def negotiate_mxp(self):
+        def enable_mxp(opt):
+            self.requestNegotiation(mxp.TELNET_OPT, '')
+
+        def no_mxp(opt):
+            pass
+
+        return self.will(mxp.TELNET_OPT).addCallbacks(enable_mxp, no_mxp)
