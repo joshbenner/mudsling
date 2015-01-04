e1ab76954f35872b0867702a37d12ee06b287234
Josh Benner <josh@bennerweb.com>
2015-01-04 12:25:48 -0500

    Add some DB abstraction framework. Not sure if I'll use it or not...

--- mudsling/utils/db.py
+++ mudsling/utils/db.py
@@ -1,5 +1,6 @@
 import yoyo
 import yoyo.connections
+import sqlite3
 
 
 def migrate(db_uri, migrations_path):
@@ -20,3 +21,68 @@ def migrate(db_uri, migrations_path):
     conn.commit()
     conn.close()
 
+
+class ExternalDatabase(object):
+    """
+    Generic external database that handles its own connection, migrations, etc.
+
+    :cvar migrations_path: Path to the migrations to execute against this kind
+        of database.
+    :type migrations_path: str
+    """
+
+    migrations_path = None
+
+    def __init__(self):
+        """
+        Initialize the connection in child implementations.
+        :return:
+        """
+        self.run_migrations(self.migrations_path)
+
+    @property
+    def db_uri(self):
+        """
+        A yoyo-migrations database URI for this database.
+        :rtype: str
+        """
+        raise NotImplementedError()
+
+    def run_migrations(self, migrations_path):
+        """
+        Execute migrations found at the specified path against this database.
+
+        :param migrations_path: The filesystem path to the yoyo files.
+        :type migrations_path: str
+        """
+        self._before_migration()
+        migrate(self.db_uri, migrations_path)
+        self._after_migration()
+
+    def _before_migration(self):
+        """
+        Optional logic before migrations. Might be useful to disconnect.
+        """
+        pass
+
+    def _after_migration(self):
+        """
+        Optional logic after migrations. Probably useful to reestablish
+        connection.
+        """
+        pass
+
+
+class SQLiteDB(ExternalDatabase):
+    """
+    Encapsulate the connection to an SQLite database.
+    """
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.connection = sqlite3.connect(filepath)
+        super(SQLiteDB, self).__init__()
+
+    @property
+    def db_uri(self):
+        return 'sqlite:///%s' % self.filepath
